947af80f42235d2c5fdc6a32319d538fc566e4f1
maria beatriz <mariabeatrizrsf@gmail.com>
2025-07-19 01:17:47 -0300

    commit final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,37 +58,42 @@ dependencies {
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth-ktx") // Adicionado para autenticação
+    implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-messaging-ktx:24.1.0")
     implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+
     // Google Sign-In (Para Social Login com Google)
-    implementation("com.google.android.gms:play-services-auth:20.7.0") // Adicionado
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     // Jetpack Compose
-    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    // Esta é a única declaração do BOM que você precisa
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("com.google.firebase:firebase-database-ktx")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.2")
+
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
     // AndroidX Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     // Testes
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00")) // Para testes
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
 }
